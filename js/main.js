6e8fc6d301a7b7488781b2fdfd1a4fcd305b4613
jdmb77 <hirschgd@gmail.com>
2013-09-21 09:01:17 -0500

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/*===================================
+Project: Apartment Ideas Mobile
+Created: 9.21.2013
+=====================================
+
+Dependencies
+-------------------------------------
+jQuery v1.9.1  
+Kendo UI Complete v2013.2.918
+-------------------------------------*/
+
+$(function(){
+  //jQuery document ready
+});
